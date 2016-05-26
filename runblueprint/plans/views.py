09e21720d51cb662f5518de2f32ee4007359157f
Mitch LeBlanc <supermitch@gmail.com>
2016-05-25 17:54:53 -0700

    Add draft from and template for plan form

--- runblueprint/plans/views.py
+++ runblueprint/plans/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
-from django.http import HttpResponse
+from .forms import PlanForm
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the plans index.")
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = PlanForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = PlanForm()
+
+    return render(request, 'plans/plan_form.html', {'form': form})
