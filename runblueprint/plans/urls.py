8812492478d49a1495d17173677cef753ec446b1
Mitch LeBlanc <supermitch@gmail.com>
2016-06-02 21:49:48 -0700

    Just make all urls live in Plan app
    Zero need to have the plan app be in a plan/ url 'folder'.

--- runblueprint/plans/urls.py
+++ runblueprint/plans/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='plans'),
-    url(r'^download/$', views.download, name='download'),
+    url(r'^$', TemplateView.as_view(template_name='plans/home.html'), name='home'),
+
+    url(r'^plans$', views.index, name='plans'),
+    url(r'^download$', views.download, name='download'),
+
+    url(r'^about$', TemplateView.as_view(template_name='plans/about.html'), name='about'),
+    url(r'^contact$', TemplateView.as_view(template_name='plans/contact.html'), name='contact'),
 ]
--- runblueprint/runblueprint/urls.py
+++ runblueprint/runblueprint/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='plans/home.html'), name='home'),
-    url(r'^about$', TemplateView.as_view(template_name='plans/about.html'), name='about'),
-    url(r'^contact$', TemplateView.as_view(template_name='plans/contact.html'), name='contact'),
-    url(r'^plans/', include('plans.urls')),
+    url(r'^', include('plans.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 
