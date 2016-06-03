4de867c7596ca6f6690e09d84dc09998d24e755c
Mitch LeBlanc <supermitch@gmail.com>
2016-06-02 22:03:32 -0700

    Add slashes to URLs
    For some reason the plans view could not resolve without the slash? Having it is better anyway.

--- runblueprint/plans/urls.py
+++ runblueprint/plans/urls.py
@@ -6,9 +6,9 @@ from . import views
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='plans/home.html'), name='home'),
 
-    url(r'^plans$', views.index, name='plans'),
-    url(r'^download$', views.download, name='download'),
+    url(r'^plans/$', views.index, name='plans'),
+    url(r'^download/$', views.download, name='download'),
 
-    url(r'^about$', TemplateView.as_view(template_name='plans/about.html'), name='about'),
-    url(r'^contact$', TemplateView.as_view(template_name='plans/contact.html'), name='contact'),
+    url(r'^about/$', TemplateView.as_view(template_name='plans/about.html'), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='plans/contact.html'), name='contact'),
 ]
