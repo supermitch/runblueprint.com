e43fa99c39bf5f7376b2c0c764d757691c34eb1d
Nigel Rahkola <me@nigelrahkola.com>
2016-06-30 13:43:25 -0700

    Create media file if it doesn't exist

--- runblueprint/plans/persister.py
+++ runblueprint/plans/persister.py
@@ -1,15 +1,17 @@
-import os.path
+import os
 
 from django.core.files import File
 from django.conf import settings
 
 from . import renderer
 
-
 def persist(plan):
     # TODO: does this work for PDF? probably not
     for format in renderer.FORMATS:
         filepath = os.path.join(settings.MEDIA_ROOT, plan.id + '.' + format)
+        d = os.path.dirname(filepath)
+        if not os.path.exists(d):
+            os.makedirs(d)
         with open(filepath, 'w') as f:
             myfile = File(f)
             myfile.write(renderer.render(plan, format))
