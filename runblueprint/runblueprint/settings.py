43122e55cb4ce22e39092404cfcdc35650079d7c
Mitch LeBlanc <supermitch@gmail.com>
2016-07-03 11:08:21 -0700

    Add ALLOWED_HOSTS to settings.py

--- runblueprint/runblueprint/settings.py
+++ runblueprint/runblueprint/settings.py
@@ -31,17 +31,11 @@ else:
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
-
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']  # SECURITY WARNING: keep the secret key used in production secret!
-logging.error('SECRET KEY IS <{}>'.format(SECRET_KEY))
-logging.error('HOSTNAME IS <{}>'.format(os.environ['RDS_USERNAME']))
-
-
 
 DEBUG = False  # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.elasticbeanstalk.com', '.elb.amazonaws.com', '.runblueprint.com']
 
 # Application definition
 INSTALLED_APPS = [
