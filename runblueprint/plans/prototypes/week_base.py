deb8a6d7450528fb74c4a9e1070cf24bf0153f58
Mitch LeBlanc <supermitch@gmail.com>
2016-09-19 21:18:42 -0700

    Add a week_base prototype file
    Added it in Python format because it's easier than JSON.

---
+++ runblueprint/plans/prototypes/week_base.py
@@ -0,0 +1,27 @@
+template = {
+    '0': {  # Monday
+        'dist': 9
+    },
+    '1': {
+        'dist': 13,
+        'type': 'strides'
+    },
+    '2': {
+        'dist': 21
+    },
+    '3': {
+        'dist': 8,
+        'type': 'strides'
+    },
+    '4': {
+        'dist': 0,
+        'type': 'x-train',
+    },
+    '5': {
+        'dist': 11
+    },
+    '6': {
+        'dist': 38,
+        'type': 'long',
+    },
+}
