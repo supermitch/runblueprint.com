4d3f098a6bcf93c2b5d2b85967b13c655c9f7276
Mitch LeBlanc <supermitch@gmail.com>
2016-09-22 22:50:30 -0700

    Add a placeholder Taper prototype

--- runblueprint/plans/prototypes/weeks.py
+++ runblueprint/plans/prototypes/weeks.py
@@ -54,6 +54,34 @@ prototypes = {
         }
     },
 
+    "Taper": {
+        0: {  # Monday
+            'percent_of_weekly_distance': 0.0875
+        },
+        1: {
+            'percent_of_weekly_distance': 0.125,
+            'type': 'strides'
+        },
+        2: {
+            'percent_of_weekly_distance': 0.2
+        },
+        3: {
+            'percent_of_weekly_distance': 0.1,
+            'type': 'strides'
+        },
+        4: {
+            'percent_of_weekly_distance': 0,
+            'type': 'x-train',
+        },
+        5: {
+            'percent_of_weekly_distance': 0.1
+        },
+        6: {
+            'percent_of_weekly_distance': 0.4,
+            'type': 'long',
+        }
+    },
+
     "Recovery": {
         0: {  # Monday
             'percent_of_weekly_distance': 0.0875
