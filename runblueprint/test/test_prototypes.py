89b70f188080ac31b687ab5db2705baedc4fbc23
Mitch LeBlanc <supermitch@gmail.com>
2016-09-26 21:43:12 -0700

    Test all prototypes have 7 days

--- runblueprint/test/test_prototypes.py
+++ runblueprint/test/test_prototypes.py
@@ -14,3 +14,8 @@ class PrototypesTestCase(TestCase):
         for name, week in weeks.prototypes.items():
             result = sum(proto['percent_of_weekly_distance'] for proto in week.values())
             self.assertAlmostEqual(result, 1, places=1, msg='while testing <{}> week'.format(name))
+
+    def test_number_of_days(self):
+        """ Make sure all weeks have 7 days. """
+        for name, week in weeks.prototypes.items():
+            self.assertEqual(len(week.keys()), 7, msg='while testing <{}> week'.format(name))
