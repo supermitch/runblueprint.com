6de0e9e15c6b0b6bb21835ab3d0ed285c2bc4812
Nigel Rahkola <jollyra@users.noreply.github.com>
2016-09-30 21:58:11 -0700

    Let's use the ORM
    The admin app depends on the ORM and I don't want to deal stripping it out or rebuilding it if we need it.

--- runblueprint/db/create.sql
+++
@@ -1,9 +0,0 @@
--- The database creation script
-
--- The runners form data
-CREATE TABLE runner_profile (
-    id bigserial primary key,
-    name text NOT NULL,
-    race_dist integer,
-    date_submitted timestamp default NULL
-);
