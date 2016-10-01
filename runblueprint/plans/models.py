e7a5d2200129ffea05a727a03980eb34fad21d37
Nigel Rahkola <jollyra@users.noreply.github.com>
2016-09-30 22:06:26 -0700

    Add a minimal model for the user's profile

--- runblueprint/plans/models.py
+++ runblueprint/plans/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+Class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    race_distance = models.IntegerField()
+
+# Class Plan(models.Model):
+# TODO how do we persist a plan? Maybe just regenerate it from the profile.
