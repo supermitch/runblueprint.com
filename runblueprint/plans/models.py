5a9dcfdffe6185ee6aa3778a3916f346454f6b4a
Nigel Rahkola <me@nigelrahkola.com>
2016-10-01 15:23:40 -0700

    Fix syntax error

--- runblueprint/plans/models.py
+++ runblueprint/plans/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
-Class Profile(models.Model):
+class Profile(models.Model):
     name = models.CharField(max_length=50)
     race_distance = models.IntegerField()
 
-# Class Plan(models.Model):
+# class Plan(models.Model):
 # TODO how do we persist a plan? Maybe just regenerate it from the profile.
