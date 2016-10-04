aac207f0a651c0ad3ad5f65693454ac3367135fb
Nigel Rahkola <me@nigelrahkola.com>
2016-10-03 22:04:39 -0700

    Persist plans as s3 urls belonging to runners

--- runblueprint/plans/models.py
+++ runblueprint/plans/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 
-class Profile(models.Model):
-    name = models.CharField(max_length=50)
-    race_distance = models.IntegerField()
+class Runner(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    created_on = models.DateTimeField(auto_now_add=True)
 
-# class Plan(models.Model):
-# TODO how do we persist a plan? Maybe just regenerate it from the profile.
+
+class Plan(models.Model):
+    runner = models.ForeignKey(Runner, on_delete=models.CASCADE)
+    s3URL = models.CharField()
+    created_on = models.DateTimeField(auto_now_add=True)
