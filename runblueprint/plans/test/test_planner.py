ad38c4b2b49c0d6d5e072cccdda361e4d9f41548
Mitch LeBlanc <supermitch@gmail.com>
2016-10-12 23:04:02 -0700

    Add tests and try inheriting from SimpleTestCase
    https://docs.djangoproject.com/en/1.10/topics/testing/tools/#django.test.SimpleTestCase

--- runblueprint/plans/test/test_planner.py
+++ runblueprint/plans/test/test_planner.py
@@ -1,18 +1,28 @@
 import datetime
 
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 from plans.planner import *
 
 
-class TestPlannerFunctions(TestCase):
+class TestPlannerFunctions(SimpleTestCase):
     def setUp(self):
-        class FormData():
-            def __init__(self):
-                self.plan_start = datetime.date('2016', '10', '11')  # Tuesday
-                self.week_day_start = '0'  # Monday
-        self.form_data = FormData()
+        pass
 
-    def test_determine_plan_start(self):
+    def test_determine_plan_start_before(self):
+        plan_start = datetime.date('2016', '10', '11')  # Tuesday
+        week_day_start = '4'  # Thursday
+        start_date = determine_plan_start(plan_start, week_day_start)
+        self.assertEqual(start_date, '2016-10-10')
+
+    def test_determine_plan_start_after(self):
+        plan_start = datetime.date('2016', '10', '11')  # Tuesday
+        week_day_start = '0'  # Monday
+        start_date = determine_plan_start(plan_start, week_day_start)
+        self.assertEqual(start_date, '2016-10-10')
+
+    def test_determine_plan_start_exact(self):
+        plan_start = datetime.date('2016', '10', '10')  # Monday
+        week_day_start = '0'  # Monday
         start_date = determine_plan_start(self.form_data.plan_start, self.form_data.week_day_start)
         self.assertEqual(start_date, '2016-10-10')
