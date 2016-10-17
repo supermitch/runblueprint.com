0a0464d199301ceda56977a1f54fd72467193012
Mitch LeBlanc <supermitch@gmail.com>
2016-10-16 22:12:31 -0700

    Add prototypes/workouts.py
    Can we pick workouts from a list? Perhaps sorted by difficulty?

---
+++ runblueprint/plans/prototypes/workouts.py
@@ -0,0 +1,11 @@
+workouts = {
+    'Tempo': [
+        '2 mi E + 2 x (20 min T + 2 min rest) + 2 mi cooldown',
+    ],
+    'Intervals': [
+        '2 mi E + 3 x (200 m R + 2 min rest) + 2 mi cooldown',
+    ],
+    'Hills': [
+        '4 x Run up a hill really fast',
+    ],
+}
