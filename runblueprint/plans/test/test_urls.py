2b3ad41dc5db3452517d7c51a7ca2c5ff61f9636
Mitch LeBlanc <supermitch@gmail.com>
2016-10-20 22:21:09 -0700

    Clean up status code tests

--- runblueprint/plans/test/test_urls.py
+++ runblueprint/plans/test/test_urls.py
@@ -1,41 +1,15 @@
-from django.test import TestCase
-from django.test import Client
+from django.test import Client, TestCase
 
 
 class TestUrls(TestCase):
     def setUp(self):
+        self.longMessage = True
         self.client = Client()  # Every test needs a client.
 
-    def test_home(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-
-    # Plan URLS
-    def test_plans(self):
-        response = self.client.get('/plans/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_about(self):
-        response = self.client.get('/about/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_contact(self):
-        response = self.client.get('/contact/')
-        self.assertEqual(response.status_code, 200)
-
-    # Registration URLs
-    def test_login(self):
-        response = self.client.get('/login/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_password_reset(self):
-        response = self.client.get('/password_reset/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_register(self):
-        response = self.client.get('/register/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_logout(self):
-        response = self.client.get('/logout/')
-        self.assertEqual(response.status_code, 200)
+    def test_status_codes_200(self):
+        urls = [
+            '', 'plans/', 'about/', 'contact/', 'login/', 'password_reset/',
+            'register/', 'logout/',
+        ]
+        for url in urls:
+            self.assertEqual(self.client.get('/' + url).status_code, 200, msg='for url <{}>'.format(url))
