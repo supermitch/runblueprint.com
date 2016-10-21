8678210c477761c6e3708723c2272094c1c6e3b7
Mitch LeBlanc <supermitch@gmail.com>
2016-10-20 22:46:47 -0700

    Add form submit integration test
    Using 'django-webtest' library.
    
    https://github.com/django-webtest/django-webtest

---
+++ runblueprint/plans/test/test_plan_form.py
@@ -0,0 +1,11 @@
+from django_webtest import WebTest
+# from django.urls import reverse
+from django.core.urlresolvers import reverse
+
+
+class AuthTest(WebTest):
+
+    def test_login(self):
+        form = self.app.get(reverse('plans')).form
+        response = form.submit().follow()
+        self.assertEqual(response.status_code, 200)
