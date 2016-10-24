9daa418ba5fa1ecc7c69fea6eb429fabd518170f
Mitch LeBlanc <supermitch@gmail.com>
2016-10-23 23:07:03 -0700

    Add mesocycles to workouts prototypes

--- runblueprint/plans/prototypes/workouts.py
+++ runblueprint/plans/prototypes/workouts.py
@@ -1,11 +1,62 @@
 workouts = {
-    'Tempo': [
-        '2 mi E + 2 x (20 min T + 2 min rest) + 2 mi cooldown',
-    ],
-    'Intervals': [
-        '2 mi E + 3 x (200 m R + 2 min rest) + 2 mi cooldown',
-    ],
-    'Hills': [
-        '4 x Run up a hill really fast',
-    ],
+    {
+        'Title': 'Phase 1: Base & Endurance',
+        'Tempo': [
+            '2 mi E + 2 x (20 min T + 2 min rest) + 2 mi cooldown',
+        ],
+        'Intervals': [
+            '2 mi E + 3 x (200 m R + 2 min rest) + 2 mi cooldown',
+        ],
+        'Hills': [
+            '4 x Run up a hill really fast',
+        ],
+    },
+    {
+        'Title': 'Phase 2: Lactate Threshold',
+        'Tempo': [
+            '2 mi E + 2 x (20 min T + 2 min rest) + 2 mi cooldown',
+        ],
+        'Intervals': [
+            '2 mi E + 3 x (200 m R + 2 min rest) + 2 mi cooldown',
+        ],
+        'Hills': [
+            '4 x Run up a hill really fast',
+        ],
+    },
+    {
+        'Title': 'Phase 3: Race Preperation',
+        'Tempo': [
+            '2 mi E + 2 x (20 min T + 2 min rest) + 2 mi cooldown',
+        ],
+        'Intervals': [
+            '2 mi E + 3 x (200 m R + 2 min rest) + 2 mi cooldown',
+        ],
+        'Hills': [
+            '4 x Run up a hill really fast',
+        ],
+    },
+    {
+        'Title': 'Phase 4: Taper & Race',
+        'Tempo': [
+            '2 mi E + 2 x (20 min T + 2 min rest) + 2 mi cooldown',
+        ],
+        'Intervals': [
+            '2 mi E + 3 x (200 m R + 2 min rest) + 2 mi cooldown',
+        ],
+        'Hills': [
+            '4 x Run up a hill really fast',
+        ],
+    },
+    {
+        'Title': 'Phase 5: Recovery',
+        'Tempo': [
+            '2 mi E + 2 x (20 min T + 2 min rest) + 2 mi cooldown',
+        ],
+        'Intervals': [
+            '2 mi E + 3 x (200 m R + 2 min rest) + 2 mi cooldown',
+        ],
+        'Hills': [
+            '4 x Run up a hill really fast',
+        ],
+    },
 }
