2051d45a18d71b529499a0e3d54c4bd7d23095c3
Mitch LeBlanc <supermitch@gmail.com>
2016-10-25 19:41:53 -0700

    Fix unit test for count_weeks_by_type
    No more string matching

--- runblueprint/plans/test/test_plan.py
+++ runblueprint/plans/test/test_plan.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from plans.planner import Plan, Week
+from plans.plan import Plan
+from plans.week import Week, Week_types
 
 
 class TestPlanMethods(TestCase):
@@ -9,15 +10,12 @@ class TestPlanMethods(TestCase):
         for i in range(5):
             week = Week(i, [])  # Empty week
             if i < 2:
-                week.type = Week.Types.Taper
+                week.type = Week_types.Taper
             else:
-                week.type = Week.Types.Recovery
+                week.type = Week_types.Recovery
             weeks.append(week)
         self.plan = Plan('Test Race', weeks)
 
     def test_count_weeks_by_type(self):
-        result = self.plan.count_weeks_by_type(Week.Types.Taper)
+        result = self.plan.count_weeks_by_type(Week_types.Taper)
         self.assertEqual(result, 2)
-
-        result = self.plan.count_weeks_by_type('Recovery')  # Works with strings
-        self.assertEqual(result, 3)
