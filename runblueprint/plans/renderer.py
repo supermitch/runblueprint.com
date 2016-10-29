c71f793065a19166092b590794bec168b6254255
Mitch LeBlanc <supermitch@gmail.com>
2016-10-28 23:31:05 -0700

    Rename max_width to day_type_width

--- runblueprint/plans/renderer.py
+++ runblueprint/plans/renderer.py
@@ -26,13 +26,15 @@ def _pdf(plan):
 def _txt(plan):
     output = str(plan)
 
-    max_width = max(len(day.type.name) for week in plan.weeks for day in week.days)
+    # Column width for Day Type
+    day_type_width = max(len(day.type.name) for week in plan.weeks for day in week.days)
+
     for week in plan.weeks:
         output += '\n\n  ' + str(week)
         for day in week.days:
             output += '\n {:>4}. {}  {}  {:<{fill}}'.format(day.number,
                     day.date.strftime('%Y-%m-%d'), day.date.strftime('%a'),
-                    day.type.name, fill=max_width)
+                    day.type.name, fill=day_type_width)
             if day.distance > 0:
                 output += ' {:>5.1f}'.format(day.distance)
             output += '  ' + day.workout
