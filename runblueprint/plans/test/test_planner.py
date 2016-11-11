47a249cf88133750bceaec5d1beca9f88d8ac6f7
Mitch LeBlanc <supermitch@gmail.com>
2016-11-11 00:30:58 -0800

    Add tests for planner needs_race_date

--- runblueprint/plans/test/test_planner.py
+++ runblueprint/plans/test/test_planner.py
@@ -27,6 +27,23 @@ class TestPlannerFunctions(SimpleTestCase):
         start_date = determine_plan_start(plan_start, week_day_start)
         self.assertEqual(start_date, datetime.date(2016, 10, 10))
 
+
+    def test_needs_race_week_recovery(self):
+        race_date = datetime.date(2016, 11, 8)  # Tue
+        start_date = datetime.date(2016, 11, 7)  # Mon
+        self.assertEqual(needs_race_week(race_date, start_date), False)
+
+    def test_needs_race_week_true(self):
+        race_date = datetime.date(2016, 11, 10)  # Thu
+        start_date = datetime.date(2016, 11, 7)  # Mon
+        self.assertEqual(needs_race_week(race_date, start_date), True)
+
+    def test_needs_race_week_taper(self):
+        race_date = datetime.date(2016, 11, 14)  # Sat
+        start_date = datetime.date(2016, 11, 7)  # Mon
+        self.assertEqual(needs_race_week(race_date, start_date), False)
+
+
     def test_determine_recovery_start_taper(self):
         start_date = datetime.date(2016, 11, 7)  # Mon
         race_date = datetime.date(2016, 11, 8)  # Tue # Should start on Monday before the race
