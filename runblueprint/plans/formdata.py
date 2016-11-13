e956157ef6b098d8b234ff027bf691a85ec3d6a1
Mitch LeBlanc <supermitch@gmail.com>
2016-11-12 17:26:10 -0800

    Stop saving self.raw_form_data

--- runblueprint/plans/formdata.py
+++ runblueprint/plans/formdata.py
@@ -8,8 +8,7 @@ class FormData():
     """
     def __init__(self, raw_form_data):
         """ Take cleaned Django form data, save it, and assign it as attrs. """
-        self.raw_data = raw_form_data
-        self.parse_raw_data(self.raw_data)
+        self.parse_raw_data(raw_form_data)
 
     def parse_raw_data(self, raw_data):
         """ Turn dict k:v pairs into instance attributes """
