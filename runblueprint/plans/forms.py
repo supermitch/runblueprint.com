95e8c930192c67164e1f941bdf5526284013c62a
Mitch LeBlanc <supermitch@gmail.com>
2016-11-19 22:46:15 -0800

    Add growth_format to plan form

--- runblueprint/plans/forms.py
+++ runblueprint/plans/forms.py
@@ -17,7 +17,6 @@ WEEKDAYS = [
     (5, 'Saturday'),
     (6, 'Sunday'),
 ]
-TAPER_LENGTHS = [('1 week', '1 week'), ('2 weeks', '2 weeks'), ('3 weeks', '3 weeks')]
 
 
 class PlanForm(forms.Form):
@@ -41,6 +40,7 @@ class PlanForm(forms.Form):
     week_day_start = forms.ChoiceField(choices=WEEKDAYS,
         initial=WEEKDAYS[0][0],
         required=True)
+
     taper_length = forms.IntegerField(initial=3,
         required=False,
         help_text='We recommend 2 or 3 weeks')
@@ -48,4 +48,6 @@ class PlanForm(forms.Form):
         required=False,
         help_text='We recommend between 5 and 8 weeks')
 
-
+    # Training options
+    growth_methods = [('step','Daniels (monthly)'), ('gradual','Gradual (weekly)')]
+    growth_method = forms.ChoiceField(choices=growth_methods, widget=forms.RadioSelect(), initial='step')
