1db9cccaeb838b92595d9e8f9c766f8b610731e5
Mitch LeBlanc <supermitch@gmail.com>
2017-11-19 00:49:37 -0800

    Update to MySQL and Django 1.11

--- runblueprint/plans/urls.py
+++ runblueprint/plans/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
 from . import views
@@ -14,4 +15,5 @@ urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name='plans/about.html'), name='about'),
     url(r'^contact/$', TemplateView.as_view(template_name='plans/contact.html'), name='contact'),
     url(r'^account/$', views.account, name='account'),
+    url(r'^login/$', auth_views.login, name="login") ,
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # TODO: Development only?
--- runblueprint/runblueprint/settings.py
+++ runblueprint/runblueprint/settings.py
@@ -65,7 +65,7 @@ LOGIN_REDIRECT_URL = 'account'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,7 +73,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.core.context_processors.media',
             ],
         },
     },
